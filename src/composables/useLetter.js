24c0371674fe9c846315b8495cce07a59a1267f0
André Baldeweg <kontakt@andrebaldeweg.de>
2022-04-03 11:53:03 +0200

    move api

--- src/composables/useLetter.js
+++ src/composables/useLetter.js
@@ -1,5 +1,6 @@
-import api from '@/api/letter'
 import { onMounted, reactive } from '@vue/composition-api'
+import { request } from '@/api'
+import file from '@/api/download'
 
 export default function useLetter() {
   const state = reactive({
@@ -8,7 +9,7 @@ export default function useLetter() {
   })
 
   const list = () => {
-    return api.list().then((response) => {
+    return request('get', '/api/letter/').then((response) => {
       state.letters = response.data
     })
   }
@@ -16,37 +17,49 @@ export default function useLetter() {
   onMounted(list)
 
   const create = (title) => {
-    return api.create({ title }).then(() => {
+    return request('post', '/api/letter/new', { title }).then(() => {
       list()
     })
   }
 
   const show = (id) => {
-    return api.show(id).then((response) => {
+    return request('get', '/api/letter/' + id).then((response) => {
       state.letter = response.data
     })
   }
 
   const update = () => {
-    return api
-      .update(state.letter.id, {
-        title: state.letter.title,
-        meta: state.letter.meta,
-        content: state.letter.content,
-      })
-      .then(() => {
-        list()
-      })
+    return request('put', '/api/letter/' + state.letter.id, {
+      title: state.letter.title,
+      meta: state.letter.meta,
+      content: state.letter.content,
+    }).then(() => {
+      list()
+    })
   }
 
   const remove = (id) => {
-    return api.remove(id).then(() => {
+    return request('delete', '/api/letter/' + id).then(() => {
       list()
     })
   }
 
   const download = (url) => {
-    return api.download(url)
+    return file()
+      .get('/api/letter/download/' + url)
+      .then((response) => {
+        const blob = new Blob([response.data], {
+          type: 'application/pdf',
+        })
+        const url = window.URL.createObjectURL(blob)
+        const a = document.createElement('a')
+        a.style.display = 'none'
+        a.href = url
+        a.download = url + '.pdf'
+        document.body.appendChild(a)
+        a.click()
+        window.URL.revokeObjectURL(url)
+      })
   }
 
   return { state, list, create, show, update, remove, download }
